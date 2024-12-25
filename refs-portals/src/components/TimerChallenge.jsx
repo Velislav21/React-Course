b1b44edf9dcb7f4f6437ea7ecc630afb2cb3d87b
Velislav21 <zirovcho34@gmail.com>
2024-12-25 20:03:05 +0200

    .
    adding the timer component

---
+++ refs-portals/src/components/TimerChallenge.jsx
@@ -0,0 +1,21 @@
+export default function TimerChallenge({ title, targetTime }) {
+
+    return (
+
+        <section className="challenge">
+            <h2>{title}</h2>
+            <p className="challenge-time">
+                {targetTime} second{targetTime > 1 ? 's' : ''}
+            </p>
+            <p>
+                <button>
+                    Start/Stop challenge
+                </button>
+            </p>
+            <p className="">
+                Time is running... / Timer inactive
+            </p>
+        </section>
+
+    )
+}
