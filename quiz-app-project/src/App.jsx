4202d06817e71e035d3e3b654efb283f51e12297
Velislav21 <zirovcho34@gmail.com>
2025-01-18 11:24:00 +0200

    .
    adding header

--- quiz-app-project/src/App.jsx
+++ quiz-app-project/src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import Header from "./components/header/Header";
+
+function App() {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
 
 export default App;
