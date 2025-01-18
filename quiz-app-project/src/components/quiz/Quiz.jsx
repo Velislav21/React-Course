0c4673d845d7299ecc0ec5da86fb5537e4691a71
Velislav21 <zirovcho34@gmail.com>
2025-01-18 19:14:15 +0200

    .
    fixing the progress bar on the timer

--- quiz-app-project/src/components/quiz/Quiz.jsx
+++ quiz-app-project/src/components/quiz/Quiz.jsx
@@ -32,7 +32,7 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
-                <QuestionTimer timeout={10000} onTimeOut={handleSkipAnswer}/>
+                <QuestionTimer timeout={10000} onTimeOut={handleSkipAnswer} key={activeQuestionIndex}/>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer) => (
--- quiz-app-project/src/components/timer/QuestionTimer.jsx
+++ quiz-app-project/src/components/timer/QuestionTimer.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react";
 
-export default function QuestionTimer({timeout, onTimeOut}) {
+export default function QuestionTimer({ timeout, onTimeOut }) {
 
     const [remainingTime, setRemainingTime] = useState(timeout);
 
     useEffect(() => {
-        setTimeout(onTimeOut, timeout)
+
+        const timer = setTimeout(onTimeOut, timeout);
+
+        return () => clearTimeout(timer);
+
     }, [timeout, onTimeOut])
 
     useEffect(() => {
-        setInterval(() => {
-            setRemainingTime((prevRemainingTime) => prevRemainingTime - 100)
+
+        const interval = setInterval(() => {
+            setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
         }, 100)
+
+        return () => clearInterval(interval);
+
     }, [])
 
 
